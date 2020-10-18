9fdd6d9657e8469601ef96c4442023d1e684f33d
hjcdb <hjcwinner@naver.com>
2020-10-18 15:05:42 +0900

    Component Register 제작

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,88 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom'
 
 const Register = () => {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
+    })
+
+    const { name, email, password, password2 } = formData
+
+    const onChange = e => {
+
+    }
+
+
     return (
-        <div>
-            <h1>Register</h1>
+       <Fragment>
+           <div className='container'>
+               <div className='row'>
+                   <div className='col-md-8 m-auto'>
+                   <h1 className='large text-primary text-center'>
+                        Sign up
+                    </h1>
+                    <p className='lead text-center'>
+                        <i className='fas fa-user' /> Create Your Account
+                    </p>
+                    <form className='form' >
+                        <div className='form-group'>
+                            <input 
+                                type='text'
+                                placeholder='Name'
+                                required
+                                name='name'
+                                value={name}
+                                onChange={e => onChange(e)}
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <input 
+                                type='email'
+                                placeholder='Email'
+                                required
+                                name='email'
+                                value={email}
+                                onChange={e => onChange(e)}
+                            />
+                            <small className='form data'>
+                                This site uses Gravatar so if you want a profile image, use a Gravatar email
+                            </small>
+                        </div>
+                        <div className='form-group'>
+                            <input 
+                                type='password'
+                                placeholder='Password'
+                                required
+                                name='password'
+                                value={password}
+                                onChange={e => onChange(e)}
+                                minLength='6'
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <input 
+                                type='password'
+                                placeholder='Confirm Password'
+                                required
+                                name='password2'
+                                value={password2}
+                                onChange={e => onChange(e)}
+                                minLength='6'
+                            />
+                        </div>
+                        <input type='submit' className='btn btn-primary' value='Register' />
+                    </form>
+                    <p className='my-1'>
+                        Already have an account? <Link to="/login">Login</Link>
+                    </p>
+                </div>
+            </div>
         </div>
+       </Fragment>
     );
 };
 
